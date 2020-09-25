c2ea9b29a65ba47817b7982bdada380d6c939cab
Viana2021 <caiomarcoschaves@gmail.com>
2020-09-25 10:46:48 -0300

    Search functions and delete function added

--- ex2/includes/searches.h
+++ ex2/includes/searches.h
@@ -1,6 +1,7 @@
 #ifndef _SEARCHES_
 #define _SEARCHES_
 
+//Struct to store the element to find related with the search type
 struct search_elem
 {
 	enum _option
@@ -15,5 +16,9 @@ struct search_elem
 typedef struct search_elem SEARCH_ELEM;
 
 SEARCH_ELEM **read_input(int array_size, int n_searches, int *array);
+int sequencial_search(int *array, int array_size, int elem);
+int bin_iterative_search(int *array, int left, int right, int elem);
+int bin_recursive_search(int *array, int left, int right, int elem);
+int search_elem_delete(SEARCH_ELEM ***search_elem, int n_searches);
 
 #endif
--- ex2/src/searches.c
+++ ex2/src/searches.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <defines.h>
 #include <searches.h>
 
 SEARCH_ELEM **read_input(int array_size, int n_searches, int *array)
@@ -19,4 +21,50 @@ SEARCH_ELEM **read_input(int array_size, int n_searches, int *array)
 	}
 
 	return search_elem;
+}
+
+int sequencial_search(int *array, int array_size, int elem)
+{
+	int i;
+	for(i = 0; i < array_size; i++)
+	{
+		if(array[i] == elem) return i + 1;
+	}
+	return NOT_FOUND;
+}
+
+int bin_iterative_search(int *array, int left, int right, int elem)
+{
+	int middle;
+	while(left <= right)
+	{
+		//always round up if middle is not an integer
+		middle = (int) ceil((double) (left + right) / 2);
+
+		if(array[middle] == elem) return middle + 1;
+		else if(array[middle] > elem) right = middle - 1;
+		else left = middle + 1;
+	}
+	return NOT_FOUND;
+}
+
+int bin_recursive_search(int *array, int left, int right, int elem)
+{
+	if(left > right) return NOT_FOUND;
+	
+	//always round up if middle is not an integer
+	int middle = (int) ceil((double) (left + right) / 2);
+
+	if(array[middle] == elem) return middle + 1;
+	else if(array[middle] > elem) return bin_recursive_search(array, left, middle - 1, elem);
+	else return bin_recursive_search(array, middle + 1, right, elem);
+	
+}
+
+int search_elem_delete(SEARCH_ELEM ***search_elem, int n_searches)
+{
+	int i;
+	for(i = 0; i < n_searches; i++) free((*search_elem)[i]);
+	free(*search_elem);
+	return SUCCESS;
 }
