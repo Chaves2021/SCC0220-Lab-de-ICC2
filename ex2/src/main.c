4fae5a0f7e1e1221359bb51b451fb448ea36f685
Viana2021 <caiomarcoschaves@gmail.com>
2020-09-25 10:47:04 -0300

    Main body finished

--- ex2/src/main.c
+++ ex2/src/main.c
@@ -8,12 +8,31 @@ int main(void)
 	int array_size, n_searches;
 	SEARCH_ELEM **search_elem;
 	int *array;
+	int i;
 
 	scanf("%d %d", &array_size, &n_searches);
 	array = (int *) malloc(array_size * sizeof(int));
 	search_elem = read_input(array_size, n_searches, array);
 
+	for(i = 0; i < n_searches; i++)
+	{
+		switch(search_elem[i]->option)
+		{
+			case SEQUENCIAL:
+				printf("%d\n", sequencial_search(array, array_size, search_elem[i]->elem));
+				break;
+			case BIN_ITERATIVE:
+				printf("%d\n", bin_iterative_search(array, 0, array_size - 1, search_elem[i]->elem));
+				break;
+			case BIN_RECURSIVE:
+				printf("%d\n", bin_recursive_search(array, 0, array_size - 1, search_elem[i]->elem));
+				break;
+			default:
+				printf("I don't know how you reached here :p\n");
+		}
+	}
 
 	free(array);
+	search_elem_delete(&search_elem, n_searches);
 	return SUCCESS;
 }
