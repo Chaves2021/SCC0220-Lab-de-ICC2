87453c01823df7347d0ac301555836e33eef554a
Viana2021 <caiomarcoschaves@gmail.com>
2020-11-27 02:23:27 -0300

    Move_box, add and remove created

--- ex5/pokebox.c
+++ ex5/pokebox.c
@@ -1,18 +1,89 @@
 #include "PokeBox.h"
 
-int movePkmn(DOUBLE_LIST HEADER *header, coord_t curPos, coord_t newPos)
+int list_elem_create(DOUBLE_LIST_HEADER *header)
+{
+	if(!header) return INVALID_LIST;
+	int counter = 0;
+	DOUBLE_LIST_ELEM **e;
+	e = &(header->first);
+
+	DOUBLE_LIST_ELEM *new = (DOUBLE_LIST_ELEM *) calloc(1, sizeof(DOUBLE_LIST_ELEM));
+	for(i = 0; i < BOXSIZE; i++) new->box[i] = NULL;
+
+	while(counter++ < header->box_count && (e = &(*e)->next));
+	*e = new;
+	new->prev = header->last;
+	header->last = new;
+	header->box_count++;
+
+	return SUCCESS;
+}
+
+DOUBLE_LIST_HEADER *box_create()
+{
+	DOUBLE_LIST_HEADER *header = (DOUBLE_LIST_HEADER *) calloc(1, sizeof(DOUBLE_LIST_HEADER));
+	_list_elem_create(header);
+	_list_elem_create(header);
+	_list_elem_create(header);
+	
+	return header;
+	
+}
+
+int remove(DOUBLE_LIST_HEADER *header)
+{
+	if(!header) return INVALID_LIST;
+	int counter = 0;
+	DOUBLE_LIST_ELEM *e = header->first;
+
+	while(counter++ < header->actual_box) e = e->next;
+
+	if(e->prev)
+		e->prev->next = e->next;
+	else
+		header->first = e->next;
+	if(e->next)
+		e->next->prev = e->prev;
+	else
+		header->last = e->prev;
+	free(e);
+	header->box_count--;
+
+	return SUCCESS;
+}
+
+int move_box(DOUBLE_LIST_HEADER *header, int move)
+{
+	if(move == -1) 
+	{
+		if(header->actual_box - 1 < 0)
+			header->actual_box = header->box_count - 1;
+		else
+			header->actual_box--;
+	}
+	else 
+	{
+		if(header->actual_box + 1 >= header->box_count)
+			header->actual_box = 0;
+		else
+			header->actual_box++;
+	}
+	return SUCCESS;
+}
+
+int pokemon_add(DOUBLE_LIST_HEADER *header, int pos)
+{
+}
+
+int movePkmn(DOUBLE_LIST_HEADER *header, coord_t curPos, coord_t newPos)
 {
     return NOONETHERE;
 }
-int addPkmn(DOUBLE_LIST HEADER *header, coord_t pos)
+int removePkmn(DOUBLE_LIST_HEADER *header, coord_t pos)
 {
     return NOONETHERE;
 }
-int removePkmn(DOUBLE_LIST HEADER *header, coord_t pos)
-{
-    return NOONETHERE;
-}
-int checkIfOccupied(DOUBLE_LIST HEADER *header, coord_t pos)
+int checkIfOccupied(DOUBLE_LIST_HEADER *header, coord_t pos)
 {
     return NOONETHERE;
 }
@@ -31,3 +102,4 @@ void printBox(pokebox_t box)
     }
     free(boxLinePokemon);
 }
+
--- ex5/pokebox.h
+++ ex5/pokebox.h
@@ -8,6 +8,9 @@
 #define BOXCOLLS 3
 #define BOXSIZE BOXLINES*BOXCOLLS
 
+#define SUCCESS		 0;
+#define INVALID_LIST 	-1;
+
 typedef enum
 {
     SUCCESS=0,
@@ -18,26 +21,32 @@ typedef enum
 
 typedef pokemon_t* pokebox_t[BOXSIZE];
 
-struct double_list
+struct double_list_elem
 {
 	pokebox_t box;
+	int pokemon_count;
 	struct double_list *prev;
 	struct double_list *next;
 };
-typedef struct double_list DOUBLE_LIST;
+typedef struct double_list_elem DOUBLE_LIST_ELEM;
 
 struct double_list_header
 {
-	DOUBLE_LIST *first;
-	DOUBLE_LIST *last;
-	int count;
+	DOUBLE_LIST_ELEM *first;
+	DOUBLE_LIST_ELEM *last;
+	int actual_box
+	int box_count;
 };
 typedef struct double_list_header DOUBLE_LIST_HEADER;
 
+DOUBLE_LIST_HEADER *box_create();
 int movePkmn(DOUBLE_LIST HEADER *header, coord_t curPos, coord_t newPos);
-int addPkmn(DOUBLE_LIST HEADER *header, coord_t pos);
 int removePkmn(DOUBLE_LIST HEADER *header, coord_t pos);
 int checkIfOccupied(DOUBLE_LIST HEADER *header, coord_t pos);
 void printBox(pokebox_t box);
+
+int box_move(DOUBLE_LIST_HEADER *header, int move);
+int list_elem_create(DOUBLE_LIST_HEADER *header);
+int list_elem_remove(DOUBLE_LIST_HEADER *header);
 
 #endif //_POKEMON_H
